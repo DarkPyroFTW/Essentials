b3587cb6ab576745585c383b182e02c126b41c7e
DarkPyroFTW <christian.musser@gmail.com>
2026-05-29 11:52:35 -0600

    TriggerBox

--- DAGV1700/AdventureGame/Assets/TriggerZone.cs
+++ DAGV1700/AdventureGame/Assets/TriggerZone.cs
@@ -3,14 +3,11 @@ using UnityEngine;
 public class TriggerZone : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        print("Triggered!");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+    
