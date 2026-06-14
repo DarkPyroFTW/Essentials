44a48097a29c70d8cd055be6038bd6fc8c989efe
DarkPyroFTW <christian.musser@gmail.com>
2026-06-13 20:55:18 -0600

    Roulette WHeel

--- DAGV1700/AdventureGame/Assets/TriggerZone.cs
+++ DAGV1700/AdventureGame/Assets/TriggerZone.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 public class TriggerZone : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void OnTriggerEnter(Collider other)
-    {
-        print("Triggered!");
-    }
+ private void OnTriggerEnter(Collider other)
+ { print ("Triggered");}
 
-    // Update is called once per frame
 }
-    
